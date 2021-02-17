38865006e6e514f5350d8c3084a9e014c064cb77
Will Roberts <willroberts3@protonmail.com>
2021-02-17 01:04:27 -0500

    org

--- ggpk.go
+++ ggpk.go
@@ -6,46 +6,74 @@ import (
 	"os"
 )
 
+// GGPK is a bin-packed container for game data.
 type GGPK struct {
 	file       *os.File
 	numRecords int32
 	offsets    []int64
 }
 
+// NewGGPK loads and initializes a GGPK file.
 func NewGGPK(filename string) (*GGPK, error) {
 	f, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
-	return &GGPK{file: f}, nil
+	g := &GGPK{file: f}
+
+	if err = g.getNumRecords(); err != nil {
+		return nil, err
+	}
+
+	if err = g.getOffsets(); err != nil {
+		return nil, err
+	}
+
+	return g, nil
 }
 
-func (g *GGPK) GetNumRecords() error {
-	buf := make([]byte, 4)
-	if _, err := g.file.Read(buf); err != nil {
+func (g *GGPK) getNumRecords() error {
+	v, err := readInt32(g.file)
+	if err != nil {
 		return err
 	}
-	var val int32
-	if err := binary.Read(bytes.NewReader(buf), binary.LittleEndian, &val); err != nil {
-		return err
-	}
-	g.numRecords = val
+	g.numRecords = v
 	return nil
 }
 
-func (g *GGPK) GetOffsets() error {
+func (g *GGPK) getOffsets() error {
 	offsets := make([]int64, 0)
 	for i := int32(0); i < g.numRecords; i++ {
-		buf := make([]byte, 8)
-		if _, err := g.file.Read(buf); err != nil {
+		v, err := readInt64(g.file)
+		if err != nil {
 			return err
 		}
-		var val int64
-		if err := binary.Read(bytes.NewReader(buf), binary.LittleEndian, &val); err != nil {
-			return err
-		}
-		offsets = append(offsets, val)
+		offsets = append(offsets, v)
 	}
 	g.offsets = offsets
 	return nil
+}
+
+func readInt32(f *os.File) (int32, error) {
+	buf := make([]byte, 4)
+	if _, err := f.Read(buf); err != nil {
+		return 0, err
+	}
+	var val int32
+	if err := binary.Read(bytes.NewReader(buf), binary.LittleEndian, &val); err != nil {
+		return 0, err
+	}
+	return val, nil
+}
+
+func readInt64(f *os.File) (int64, error) {
+	buf := make([]byte, 8)
+	if _, err := f.Read(buf); err != nil {
+		return 0, err
+	}
+	var val int64
+	if err := binary.Read(bytes.NewReader(buf), binary.LittleEndian, &val); err != nil {
+		return 0, err
+	}
+	return val, nil
 }
--- ggpk_test.go
+++ ggpk_test.go
@@ -5,14 +5,10 @@ import (
 )
 
 func TestGGPK(t *testing.T) {
-	f, err := NewGGPK("G:\\Path of Exile\\Content.ggpk")
+	//f, err := NewGGPK("G:\\Path of Exile\\Content.ggpk")
+	f, err := NewGGPK("C:\\Program Files (x86)\\Grinding Gear Games\\Path of Exile\\Content.ggpk")
 	if err != nil {
 		t.Fatal(err)
 	}
-	if err = f.GetNumRecords(); err != nil {
-		t.Fatal(err)
-	}
-	if err = f.GetOffsets(); err != nil {
-		t.Fatal(err)
-	}
+	t.Log(f)
 }
