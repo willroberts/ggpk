77c74b047993c9f4f7f32a416fec68d58177faba
Will Roberts <willroberts3@protonmail.com>
2025-07-22 20:24:18 -0400

    Normalizes file path in unit test

--- ggpk_test.go
+++ ggpk_test.go
@@ -1,15 +1,9 @@
 package ggpk
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestGGPK(t *testing.T) {
-	var (
-		path = "G:\\Path of Exile\\Content.ggpk" // desktop
-		//path = "C:\\Program Files (x86)\\Grinding Gear Games\\Path of Exile\\Content.ggpk" // laptop
-	)
-
+	var path = "C:\\Program Files (x86)\\Grinding Gear Games\\Path of Exile\\Content.ggpk"
 	if _, err := NewGGPK(path); err != nil {
 		t.Fatal(err)
 	}
